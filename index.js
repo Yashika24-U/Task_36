b61bf7c6cab73f0144307dde54980db07395e08b
YashikaUvaraj <yashikayeshi@gmail.com>
2023-09-06 13:08:00 +0530

    updated

--- index.js
+++ index.js
@@ -38,12 +38,13 @@ app.post("/createRoom",async function(req,res){
 
 // ==========================================================================================
 app.post("/BookRoom",async function(req,res){
-    const { roomId, customerName, date, startTime, endTime } = req.body;
+    const { roomId, customerName, date, startTime, endTime } = req.body[0];
+    console.log(req.body)
 
     // Find the room with the specified roomId
     try {
         const room = await client.db("task").collection("room").findOne({ id: roomId })
-    
+       //console.log(typeof(roomId))
         if (!room) {
           return res.status(404).json({ error: 'Room not found.' });
         }
@@ -52,39 +53,80 @@ app.post("/BookRoom",async function(req,res){
           return res.status(400).json({ error: 'Room is already booked.' });
         }
     
-        await room.updateOne({ id: roomId }, { $set: { Booked: 'True' } });
+        await client.db("task").collection("room").updateOne({ id: roomId }, { $set: { Booked: 'True' } });
 
+        console.log("updation done")
+      const booking = {
+      customer_name: customerName,
+      date,
+      start_time: startTime,
+      end_time: endTime,
+      roomId: roomId,
+    };
+
+    const bookingResult = await client
+      .db("task")
+      .collection("booking")
+      .insertOne(booking)
+      
     
-    
-        const booking = await booking.create({
-          customer_name: customerName,
-          date,
-          start_time: startTime,
-          end_time: endTime,
-          room: room._id,
-        });
-        await db.collection('booking').insertOne(booking);
-    
+         console.log("booking done")
         res.json({ message: 'Room booked successfully.', room, booking });
       } catch (error) {
-        res.status(500).json({ error: 'An error occurred while booking the room.' });
+        res.status(500).json(error);
       }
     }); 
- 
 
     // ==========================================================================================
  
 
-    app.get("/BookedRooms", async function (req, resp) {
+    // app.get("/BookedRooms", async function (req, resp) {
      
-      const result = await client
-        .db("task")
-        .collection("booking")
-        .find({_id:0,roomId:1})
-        .toArray();
+    //   const result = await client
+    //     .db("task")
+    //     .collection("booking")
+    //     // .find({_id:0,roomId:1})
+    //     .find({}, { _id: 0, roomId: 1 });
+
+       
     
-      result ? resp.send(result) : resp.status(404).send("No rooms are booked!!");
+    //   result ? resp.send(result) : resp.status(404).send("No rooms are booked!!");
+    // });
+
+
+
+
+
+
+
+
+
+    app.get("/BookedRooms", async function (req, resp) {
+      try {
+        const cursor = await client
+          .db("task")
+          .collection("booking")
+          .find({}, { _id: 0, roomId: 1 }); // Projection to include only roomId
+    
+        const bookedRooms = await cursor.toArray();
+    
+        if (bookedRooms.length === 0) {
+          resp.status(404).send("No rooms are booked!!");
+        } else {
+          resp.json(bookedRooms);
+        }
+      } catch (error) {
+        console.error("Error retrieving booked rooms:", error);
+        resp.status(500).send("Internal Server Error");
+      }
     });
+ 
+    
+    
+    
+    
+    
+    
     
 
      // db.booking.find({}, { _id: 0, room_id: 1 })
@@ -93,16 +135,28 @@ app.post("/BookRoom",async function(req,res){
   
  
  
- app.get("/ListCustomers", async function (req, resp) {
+//  app.get("/ListCustomers", async function (req, resp) {
+  app.get("/ListCustomers", async function (req, resp) {
+    try {
+      const result = await client
+        .db("task")
+        .collection("booking")
+        .distinct("customer_name");
+  
+      if (result.length === 0) {
+        resp.status(404).send("No customers have made bookings!!");
+      } else {
+        resp.json(result);
+      }
+    } catch (error) {
+      console.error("Error listing customers:", error);
+      resp.status(500).send("Internal Server Error");
+    }
+  });
+  
      
-  const result = await client
-    .db("task")
-    .collection("booking")
-    .distinct("customer_name", { room_id: { $exists: true } })
-    .toArray();
+  
 
-  result ? resp.send(result) : resp.status(404).send("No rooms are booked!!");
-});
 //=========================================================================================
 //List how many times a customer has booked the room with below details
 
@@ -111,13 +165,42 @@ app.post("/BookRoom",async function(req,res){
  
  
  
- 
- 
- 
- 
- 
- 
- 
+app.get("/CustomerBookingCounts", async function (req, resp) {
+  try {
+    const result = await client
+      .db("task")
+      .collection("booking")
+      .aggregate([
+        {
+          $group: {
+            _id: "$customer_name", // Group by customer name
+            count: { $sum: 1 }, // Count the number of bookings
+            bookings: {
+              $push: {
+                // Include booking details
+                room: "$room",
+                date: "$date",
+                start_time: "$start_time",
+                end_time: "$end_time",
+              },
+            },
+          },
+        },
+      ])
+      .toArray();
+
+    if (result.length === 0) {
+      resp.status(404).send("No customers have made bookings!!");
+    } else {
+      resp.json(result);
+    }
+  } catch (error) {
+    console.error("Error listing customer booking counts:", error);
+    resp.status(500).send("Internal Server Error");
+  }
+});
+
+
  //======================================================================================
 
 
